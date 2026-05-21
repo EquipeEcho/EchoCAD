da8cda122b8f386d6fe8ee707bd6bf686e516db5
Wesley Xavier <wesley.xvier@gmail.com>
2026-05-21 11:46:33 -0300

    FIX: correção definitiva aplicada no docker-compose

---
+++ app/backend/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Aborta o script se algum comando falhar
+set -e
+
+echo "Rodando migrações do Alembic..."
+alembic upgrade head
+
+echo "Iniciando a aplicação..."
+# Executa o comando que foi passado no CMD do Dockerfile
+exec "$@"
