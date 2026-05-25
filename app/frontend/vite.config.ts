ea9e2b13f8b2ae8a52f66b9abff06962f69e5a09
nicolasffe <nicolasferreira.ismart@gmail.com>
2026-05-25 12:05:42 -0300

    fix: configurar proxy da api no vite

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -18,5 +18,12 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 });
